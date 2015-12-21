6b8e56c261235e184e58341a1a91fd6585234cc4
Anna Horstmann <ahorstmann@phytec.com>
2015-12-21 11:05:42 -0800

    configs: mx7d_phyboard: update name of DTB file to "imx7-phyboard-rdk.dtb"
    Signed-off-by: Anna Horstmann <ahorstmann@phytec.com>
    (cherry picked from commit b85fc9ff2d96029c85d4e98cd6efd77437cf2b47)
    (cherry picked from commit f97d90c027a8f729a29dd174313b89cff95c3ff7)
    (cherry picked from commit 43e218ffc93a00ac8b5eeb7b242c3cf308034fa7)

--- include/configs/mx7d_phyboard.h
+++ include/configs/mx7d_phyboard.h
@@ -186,7 +186,7 @@
 	"console=ttymxc4\0" \
 	"fdt_high=0xffffffff\0" \
 	"initrd_high=0xffffffff\0" \
-	"fdt_file=imx7d-sdb.dtb\0" \
+	"fdt_file=imx7-phyboard-rdk.dtb\0" \
 	"fdt_addr=0x83000000\0" \
 	"boot_fdt=try\0" \
 	"ip_dyn=yes\0" \
