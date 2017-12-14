8f734a7f1255f08c64193ec52f1f6b0c8dd5234b
Anna Horstmann <ahorstmann@phytec.com>
2017-12-14 15:38:01 -0800

    board: phytec: mx7d_phyboard_zeta: remove unused uart1 pinmuxing
    Signed-off-by: Anna Horstmann <ahorstmann@phytec.com>

--- board/phytec/mx7d_phyboard_zeta/mx7d_phyboard_zeta.c
+++ board/phytec/mx7d_phyboard_zeta/mx7d_phyboard_zeta.c
@@ -57,11 +57,6 @@ static iomux_v3_cfg_t const wdog_pads[] = {
 	MX7D_PAD_GPIO1_IO00__WDOG1_WDOG_B | MUX_PAD_CTRL(NO_PAD_CTRL),
 };
 
-static iomux_v3_cfg_t const uart1_pads[] = {
-	MX7D_PAD_UART1_TX_DATA__UART1_DCE_TX | MUX_PAD_CTRL(UART_PAD_CTRL),
-	MX7D_PAD_UART1_RX_DATA__UART1_DCE_RX | MUX_PAD_CTRL(UART_PAD_CTRL),
-};
-
 static iomux_v3_cfg_t const uart5_pads[] = {
 	MX7D_PAD_GPIO1_IO07__UART5_TX_DATA | MUX_PAD_CTRL(UART_PAD_CTRL),
 	MX7D_PAD_GPIO1_IO06__UART5_RX_DATA | MUX_PAD_CTRL(UART_PAD_CTRL),
@@ -113,7 +108,6 @@ static void setup_gpmi_nand(void)
 
 static void setup_iomux_uart(void)
 {
-/*	imx_iomux_v3_setup_multiple_pads(uart1_pads, ARRAY_SIZE(uart1_pads)); */
 	imx_iomux_v3_setup_multiple_pads(uart5_pads, ARRAY_SIZE(uart5_pads));
 }
 
